7e86607054711d1bf79cffbf3eeddeb3599944fa
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-08-12 22:15:53 +0300

    feat: add login and logout functionality

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service'
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   // true for dev, change on fasle when build app
   public isFilterBlockVisible: boolean = true;
 
-  constructor() { }
+  public activeUserName: User | null = null;
+
+  constructor(private userServise: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.userServise.getUserObservable()
+      .subscribe(newUser => {
+        this.activeUserName = newUser;
+      });
+  }
+
+  public logOut(): void {
+    this.userServise.logOut();
+    this.router.navigate(['auth']);
+  }
+
+  public goToAuth(): void {
+    this.router.navigate(['auth']);
+  }
+
+  public goToHome(): void {
+    this.router.navigate(['/']);
+  }
 
 }
