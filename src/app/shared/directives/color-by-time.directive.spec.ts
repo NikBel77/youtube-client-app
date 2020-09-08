570bd8dfefdc18085b86e8c6bf09435fef906e95
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-09-08 19:41:29 +0300

    unit tests: test for shared module

--- src/app/shared/directives/color-by-time.directive.spec.ts
+++ src/app/shared/directives/color-by-time.directive.spec.ts
@@ -1,9 +1,25 @@
 import { ColorByTimeDirective } from './color-by-time.directive';
 import { ElementRef } from '@angular/core';
+import { colors } from '../models/color-by-time.model';
 
 describe('ColorByTimeDirective', () => {
+  let directive: ColorByTimeDirective;
+
+  beforeEach(() => {
+    directive = new ColorByTimeDirective(new ElementRef('dit'));
+  });
+
   it('should create an instance', () => {
-    const directive: ColorByTimeDirective = new ColorByTimeDirective(new ElementRef('div'));
     expect(directive).toBeTruthy();
+  });
+
+  it('colorByTime should return color depends on time', () => {
+    const actualColor: string = directive.getColorByTime(new Date());
+    const weekColor: string = directive.getColorByTime(new Date(Date.now() - new Date(0).setDate(10)));
+    const monthColor: string = directive.getColorByTime(new Date(Date.now() - new Date(0).setMonth(1)));
+
+    expect(actualColor).toBe(colors.blue);
+    expect(weekColor).toBe(colors.green);
+    expect(monthColor).toBe(colors.red);
   });
 });
--- src/app/shared/pipes/filter-cards.pipe.spec.ts
+++ src/app/shared/pipes/filter-cards.pipe.spec.ts
@@ -1,8 +1,82 @@
 import { FilterCardsPipe } from './filter-cards.pipe';
+import { IFilterSettings } from '../models/filter-settings.model';
+import { ICollectionItem } from 'src/app/redux/state.models';
 
 describe('FilterCardsPipe', () => {
+  let filterSettings: IFilterSettings;
+  let pipe: FilterCardsPipe;
+  beforeEach(() => {
+    filterSettings = {
+      filterBy: 'NONE',
+      isReverse: false,
+      keyWord: ''
+    };
+    pipe = new FilterCardsPipe();
+  });
+
   it('create an instance', () => {
-    const pipe: FilterCardsPipe = new FilterCardsPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should filter by key word', () => {
+    const cards: ICollectionItem[] = [
+      { item: { snippet: { title: 'title1', description: 'description1' } } } as ICollectionItem,
+      { item: { snippet: { title: 'title2', description: 'description2' } } } as ICollectionItem,
+      { item: { snippet: { title: 'title3', description: 'description3' } } } as ICollectionItem,
+    ];
+    filterSettings.keyWord = 'title1';
+    const filtered: ICollectionItem[] = pipe.transform(cards, filterSettings);
+
+    expect(filtered).toEqual([cards[0]]);
+  });
+
+  it('should do nothing with initial settings', () => {
+    const cards: ICollectionItem[] = [
+      { item: { snippet: { title: 'title1', description: 'description1' } } } as ICollectionItem,
+      { item: { snippet: { title: 'title2', description: 'description2' } } } as ICollectionItem,
+      { item: { snippet: { title: 'title3', description: 'description3' } } } as ICollectionItem,
+    ];
+    const filtered: ICollectionItem[] = pipe.transform(cards, filterSettings);
+
+    expect(filtered).toEqual(cards);
+  });
+
+  it('should sort by time', () => {
+    const cards: ICollectionItem[] = [
+      { item: { snippet: { publishedAt: new Date(30).toDateString() } } } as ICollectionItem,
+      { item: { snippet: { publishedAt: new Date(10).toDateString() } } } as ICollectionItem,
+      { item: { snippet: { publishedAt: new Date(20).toDateString() } } } as ICollectionItem,
+    ];
+    filterSettings.filterBy = 'DATE';
+    const filtered: ICollectionItem[] = pipe.transform(cards, filterSettings);
+
+    expect(filtered).toEqual([cards[1], cards[2], cards[0]]);
+  });
+
+  it('should reverse sort by time', () => {
+    const cards: ICollectionItem[] = [
+      { item: { snippet: { publishedAt: new Date(30).toDateString() } } } as ICollectionItem,
+      { item: { snippet: { publishedAt: new Date(10).toDateString() } } } as ICollectionItem,
+      { item: { snippet: { publishedAt: new Date(20).toDateString() } } } as ICollectionItem,
+    ];
+    filterSettings.filterBy = 'DATE';
+    filterSettings.isReverse = true;
+    const filtered: ICollectionItem[] = pipe.transform(cards, filterSettings);
+
+    expect(filtered).toEqual([cards[0], cards[2], cards[1]]);
+  });
+
+  it('should sort by view', () => {
+    const cards: ICollectionItem[] = [
+      { item: {}, isCustom: true } as ICollectionItem,
+      { item: { statistics: { viewCount: '50' } }, isCustom: false } as ICollectionItem,
+      { item: { statistics: { viewCount: '20'} }, isCustom: false } as ICollectionItem,
+      { item: { statistics: { viewCount: '100' } }, isCustom: false } as ICollectionItem,
+    ];
+    filterSettings.filterBy = 'VIEW';
+    const filtered: ICollectionItem[] = pipe.transform(cards, filterSettings);
+
+    expect(filtered).toEqual([cards[3], cards[1], cards[2], cards[0]]);
+  });
+
 });
